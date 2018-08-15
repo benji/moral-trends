d3e04849bbdeeac58e7688099e832a9d5a765add
Benjamin Jaton <benjamin.jaton@gmail.com>
2018-08-15 11:37:33 -0700

    Merging no opinion and depends

--- process_raw_html.py
+++ process_raw_html.py
@@ -13,6 +13,14 @@ soup = BeautifulSoup(html, 'html.parser')
 
 data = {}
 
+
+def read_pct_int(row, header):
+    text = row.find('td', attrs={'data-th': header}).text
+    if text == '*':
+        return 0
+    return int(text)
+
+
 for section in soup.find_all('tbody', attrs={'class': 'row-group'}):
     title = section.find('th', attrs={'scope': 'rowgroup'}).text
     print 'Title: [{}]'.format(title)
@@ -20,7 +28,7 @@ for section in soup.find_all('tbody', attrs={'class': 'row-group'}):
     years = []
     pct_pos_arr = []
     pct_neg_arr = []
-    pct_depends_arr = []
+    pct_other_arr = []
 
     first_row = True
     for row in section.find_all('tr'):
@@ -31,31 +39,27 @@ for section in soup.find_all('tbody', attrs={'class': 'row-group'}):
         date = row.find('th', attrs={'scope': 'row'}).text
         year = re.sub(' .*', '', date)
 
-        pct_positive = row.find(
-            'td', attrs={'data-th': 'Morally acceptable'}).text
-        pct_negative = row.find(
-            'td', attrs={'data-th': 'Morally wrong'}).text
-        pct_depends = row.find(
-            'td', attrs={'data-th': 'Depends (vol.)'}).text
-
-        if pct_depends == '*':
-            pct_depends = 0
+        pct_positive = read_pct_int(row, 'Morally acceptable')
+        pct_negative = read_pct_int(row, 'Morally wrong')
+        pct_depends = read_pct_int(row, 'Depends (vol.)')
+        pct_no_opinion = read_pct_int(row, 'No opinion')
+        pct_other = pct_depends+pct_no_opinion
 
         print '\t{} - Good:{}% Bad:{}% Depends:{}%'.format(
             year, pct_positive, pct_negative, pct_depends)
 
-        years.insert(0,int(year))
-        pct_pos_arr.insert(0,int(pct_positive))
-        pct_neg_arr.insert(0,int(pct_negative))
-        pct_depends_arr.insert(0,int(pct_depends))
+        years.insert(0, year)
+        pct_pos_arr.insert(0, pct_positive)
+        pct_neg_arr.insert(0, pct_negative)
+        pct_other_arr.insert(0, pct_depends+pct_no_opinion)
 
     if len(years) > 1:
-      data[title] = {
-          'years': years,
-          'good': pct_pos_arr,
-          'bad': pct_neg_arr,
-          'depends': pct_depends_arr
-      }
+        data[title] = {
+            'years': years,
+            'good': pct_pos_arr,
+            'bad': pct_neg_arr,
+            'other': pct_other_arr
+        }
 
 with open('data.js', 'w') as outfile:
     jsonstr = json.dumps(data)
